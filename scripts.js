d316dab9aac2dfabe293593336cec03202f1bfa7
Carlos Melo <106975128+carlosmelo35@users.noreply.github.com>
2022-07-05 15:36:59 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function zerar() {
+	count = 0;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
